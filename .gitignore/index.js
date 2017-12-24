bbb652008ae5fc97e3978750a7bdaafdd547b214
romtec <34814622+romtec@users.noreply.github.com>
2017-12-24 16:31:23 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client()
+
+bot.on("ready", () => {
+    bot.user.setGame({ game: {name: '|d!help|', type : 0}})
+    console.log("Bot Ready !");
+});
+
+bot.login("Mzk0NTEwMDAwOTc5OTY4MDAx.DSFXlA.dp7JkFA1_JhPMkdqsZTWima3ysw")
