884d178ae22cca41819cdc1676c6aa4f3924b9ba
romtec <34814622+romtec@users.noreply.github.com>
2017-12-25 12:39:17 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -27,6 +27,39 @@ if(message.content == prefix + "trouverlechiffre"){
 
 }
 
+    if(message.content == prefix + "help"){
+
+    message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "help",
+    description: "Aide",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+});
+    
+}
+
 if(party_launch && message.content != null){
 
 if(Number.isInteger(parseInt(message.content))){
